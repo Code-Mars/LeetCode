c2d75aebaf6d0ae43369d8315dbad33595bc3257
Chandrabhan Maurya <141003353+Code-Mars@users.noreply.github.com>
2024-08-26 10:38:05 +0530

    Time: 11 ms (78.54%), Space: 15.4 MB (21.73%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int>ans;
+    void dfs(Node* root){
+        if(!root)return;
+        for(auto x:root->children)dfs(x);
+        ans.push_back(root->val);
+    }
+    vector<int> postorder(Node* root) {
+        dfs(root);
+        return ans;
+    }
+};
